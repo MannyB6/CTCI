39ac2dff46714c5856e9ce158a7f2b278bb61d0e
MannyB6 <43554936+MannyB6@users.noreply.github.com>
2020-07-13 18:47:01 -0700

    Adding bigO answers from CTCI 6th Edition

---
+++ bigO/bigO.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "bigO.h"
+
+using namespace std;
+
+
+
+int main() {
+
+    answerOne();
+    answerTwo();
+    answerThree();
+    answerFour();
+    answerFive();
+    answerSix();
+    answerSeven();
+    answerEight();
+    answerNine();
+    answerTen();
+    answerEleven();
+    answerTwelve();
+}
+
+
+
+
+
---
+++ bigO/bigO.h
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+void answerOne() {
+    cout << "1. The runtime for V1.1 is: O(b)" << endl;
+}
+
+void answerTwo() {
+    cout << "2. The runtime for V1.2 is: O(b)" << endl;
+}
+
+void answerThree() {
+    cout << "3. The runtime for V1.3 is: O(1)" << endl; 
+}
+
+void answerFour() {
+    cout << "4. The runtime is: O(a / b)" << endl;
+}
+
+void answerFive() {
+    cout << "5. The runtime is: O(log(n))" << endl; // binary search
+}
+
+void answerSix() {
+    cout << "6. The runtime is: O(sqrt(n))" << endl;
+}
+
+void answerSeven() {
+    cout << "7. The runtime is: O(n)" << endl;
+}
+
+void answerEight() {
+    cout << "8. The runtime is: O(n)" << endl;
+}
+
+void answerNine(){
+    cout << "9. The runtime is: O(n^2)" << endl;
+}
+
+void answerTen() {
+    cout << "10. The runtime is: O(log(n))" << endl;
+}
+
+void answerEleven(){
+    cout << "11. The runtime is: O(k*c^k)" << endl;
+}
+
+void answerTwelve() {
+    cout << "12. The runtime is: O(b * log(b) + a * log(b))" << endl;
+}
